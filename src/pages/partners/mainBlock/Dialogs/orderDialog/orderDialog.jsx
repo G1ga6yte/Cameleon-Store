7f8b981e0915105ad536e10006cfccb210ff0a87
Vache <gigabyte10tb@gmail.com>
2025-04-09 18:52:23 +0400

    update vs code

--- src/pages/partners/mainBlock/Dialogs/orderDialog/orderDialog.jsx
+++ src/pages/partners/mainBlock/Dialogs/orderDialog/orderDialog.jsx
@@ -1,21 +1,31 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './OrderDialog.scss';
-import {useTranslation} from "react-i18next";
+import { useTranslation } from "react-i18next";
 import { IoClose } from "react-icons/io5";
 import { HiMiniSquares2X2 } from "react-icons/hi2";
-import { BsTrash3 } from "react-icons/bs";
+import { BsTrash3Fill } from "react-icons/bs";
 import OrderDialogListMenu from "../orderDialogListMenu/orderDialogListMenu.jsx";
-
-function OrderDialog({setOrderDialog}) {
-    const {t} = useTranslation();
+import { TestOrderList } from "./testOrderList.js"
+function OrderDialog({ setOrderDialog }) {
+    const { t } = useTranslation();
     const id = 345
 
     const [listMenuDialog, setListMenuDialog] = useState(false);
     const [productsList, setProductsList] = useState([]);
 
     useEffect(() => {
-        
+        setProductsList(TestOrderList)
     }, [])
+
+
+    const removeProduct = (index) => {
+        const updatedProductsList = productsList.filter((_, i) => i !== index);
+        setProductsList(updatedProductsList);
+    };
+
+    const getTotalPrice = () => {
+        return productsList.reduce((total, item) => total + (item.price * item.peaces), 0);
+    };
 
     return (
         <div className='OrderDialogContainer'>
@@ -24,37 +34,37 @@ function OrderDialog({setOrderDialog}) {
                 setListMenuDialog={setListMenuDialog}
                 setProductsList={setProductsList}
             />}
-            <div onClick={()=>setOrderDialog(false)} className="backgroundBlock"></div>
+            <div onClick={() => setOrderDialog(false)} className="backgroundBlock"></div>
 
             <div className="OrderDialog G-box-shadow">
                 <div className="headLine">
                     <p className="header">{t("partners.header3")} #{id}</p>
-                    <button onClick={()=>setOrderDialog(false)} className="closeBtn">
-                        <IoClose className="icon"/>
+                    <button onClick={() => setOrderDialog(false)} className="closeBtn">
+                        <IoClose className="icon" />
                     </button>
                 </div>
 
                 <div className="inputsContainer">
-                        <input
-                            type="text"
-                            className="searchInput"
-                            placeholder={t("partners.placeholder3")}
-                        />
-                        <input
-                            type="text"
-                            className="searchInput"
-                            placeholder={t("partners.placeholder4")}
-                        />
+                    <input
+                        type="text"
+                        className="searchInput"
+                        placeholder={t("partners.placeholder3")}
+                    />
+                    <input
+                        type="text"
+                        className="searchInput"
+                        placeholder={t("partners.placeholder4")}
+                    />
 
-                    <button onClick={()=>{setListMenuDialog(true)}} className="toggleListMenu">
-                        <HiMiniSquares2X2 className="icon"/>
+                    <button onClick={() => { setListMenuDialog(true) }} className="toggleListMenu">
+                        <HiMiniSquares2X2 className="icon" />
                         {t("partners.btn3")}
                     </button>
                 </div>
 
                 <div className="ListContainer G-scrollbar-style">
-                        <table className="table">
-                            <thead className="tableHead">
+                    <table className="table">
+                        <thead className="tableHead">
                             <tr className="tableHeadRow">
                                 <th>#№</th>
                                 <th>#{t("partners.th1")}</th>
@@ -62,40 +72,82 @@ function OrderDialog({setOrderDialog}) {
                                 <th>#{t("partners.th8")}</th>
 
                                 <th>#{t("partners.th9")}</th>
-                                <th>#{t("partners.th5")}</th>
+                                <th>#{t("partners.th11")}</th>
                                 <th>#{t("partners.th10")}</th>
                                 <th></th>
 
                             </tr>
-                            </thead>
-                            <tbody className="tableBody">
+                        </thead>
+                        <tbody className="tableBody">
 
 
                             {productsList.map((item, index) => {
 
                                 return (
                                     <tr className={`tableRow`} key={index}>
-                                        <td>{index+1}</td>
-                                        <td>{item.regularNum}</td>
+                                        <td className='indexTD'>{index + 1}</td>
+                                        <td>{item._id}</td>
                                         <td>{item.article}</td>
                                         <td>{item.name}</td>
-                                        <td>{item.sellingSum}.00</td>
-                                        <td>{item.profit}.00</td>
-                                        <td>{item.total}.00</td>
+                                        <td>{item.price}.00</td>
                                         <td>
-                                            <button className="removeBtn">
-                                                <BsTrash3 className="icon"/>
+                                            <input
+                                                type="text"
+                                                className="peacesInput"
+                                                value={item.peaces}
+                                                onChange={(e) => {
+                                                    const updatedPeaces = e.target.value;
+                                                    const updatedProductsList = [...productsList];
+                                                    updatedProductsList[index].peaces = parseInt(updatedPeaces, 10) || 0;
+                                                    setProductsList(updatedProductsList);
+                                                }}
+                                            />
+                                        </td>
+                                        <td className="totalTD">{item.price * item.peaces}.00</td>
+                                        <td>
+                                            <button className="removeBtn" onClick={() => removeProduct(index)}>
+                                                <BsTrash3Fill className="icon" />
                                             </button>
                                         </td>
 
                                     </tr>
-                                )})}
-                            </tbody>
+                                )
+                            })}
+                        </tbody>
 
-                        </table>
+                    </table>
 
 
-                        <div className="overflowWhiteBlock"></div>
+                    
+
+
+                    <div className="overflowWhiteBlock"></div>
+
+                    <div className="totalCont">
+                        <div className="infoCont">
+                            <div className="prgLine">
+                                <span>{t("partners.prg7")}</span>
+                                <div className="line"></div>
+                                <span>{getTotalPrice().toFixed(2)}</span>
+                            </div>
+
+                            <div className="prgLine">
+                                <span>{t("partners.prg8")}</span>
+                                <div className="line"></div>
+                                <span>0.00</span>
+                            </div>
+
+                            <div className="prgLine">
+                                <span>{t("partners.prg8")}</span>
+                                <div className="line"></div>
+                                <span>{getTotalPrice().toFixed(2)}</span>
+                            </div>
+                        </div>
+
+                        <div className="actionsCont">
+
+                        </div>
+                    </div>
                 </div>
 
 
---
+++ src/pages/partners/mainBlock/Dialogs/orderDialog/testOrderList.js
@@ -0,0 +1,142 @@
+export const TestOrderList = [
+    {
+        _id: "84629464262",
+        article: "587090",
+        name: "Plastic Primer 500",
+        price: 4000,
+        peaces: 1
+    },
+    {
+        _id: "84629464263",
+        article: "587091",
+        name: "Metallic Spray Paint",
+        price: 2500,
+        peaces: 2
+    },
+    {
+        _id: "84629464264",
+        article: "587092",
+        name: "Wooden Sealant 200",
+        price: 1500,
+        peaces: 5
+    },
+    {
+        _id: "84629464265",
+        article: "587093",
+        name: "Acrylic Paint Set",
+        price: 3200,
+        peaces: 3
+    },
+    {
+        _id: "84629464266",
+        article: "587094",
+        name: "Car Tire Cleaner",
+        price: 1800,
+        peaces: 4
+    },
+    {
+        _id: "84629464267",
+        article: "587095",
+        name: "Wood Stain Dark Brown",
+        price: 2200,
+        peaces: 2
+    },
+    {
+        _id: "84629464268",
+        article: "587096",
+        name: "Leather Conditioner",
+        price: 1200,
+        peaces: 6
+    },
+    {
+        _id: "84629464269",
+        article: "587097",
+        name: "Ceramic Glaze 300ml",
+        price: 950,
+        peaces: 3
+    },
+    {
+        _id: "84629464270",
+        article: "587098",
+        name: "Glass Cleaner 500ml",
+        price: 650,
+        peaces: 7
+    },
+    {
+        _id: "84629464271",
+        article: "587099",
+        name: "Auto Polish Wax",
+        price: 3000,
+        peaces: 1
+    },
+    {
+        _id: "84629464272",
+        article: "587100",
+        name: "Hand Sanitizer 200ml",
+        price: 500,
+        peaces: 10
+    },
+    {
+        _id: "84629464273",
+        article: "587101",
+        name: "Industrial Mask 3M",
+        price: 750,
+        peaces: 8
+    },
+    {
+        _id: "84629464274",
+        article: "587102",
+        name: "Painter's Tape Roll",
+        price: 300,
+        peaces: 15
+    },
+    {
+        _id: "84629464275",
+        article: "587103",
+        name: "Car Upholstery Cleaner",
+        price: 2000,
+        peaces: 4
+    },
+    {
+        _id: "84629464276",
+        article: "587104",
+        name: "Clear Coat Spray",
+        price: 3500,
+        peaces: 2
+    },
+    {
+        _id: "84629464277",
+        article: "587105",
+        name: "Roof Sealant Black",
+        price: 1700,
+        peaces: 5
+    },
+    {
+        _id: "84629464278",
+        article: "587106",
+        name: "Paint Roller Set",
+        price: 900,
+        peaces: 6
+    },
+    {
+        _id: "84629464279",
+        article: "587107",
+        name: "Outdoor Furniture Paint",
+        price: 2200,
+        peaces: 3
+    },
+    {
+        _id: "84629464280",
+        article: "587108",
+        name: "Metal Rust Remover",
+        price: 1200,
+        peaces: 2
+    },
+    {
+        _id: "84629464281",
+        article: "587109",
+        name: "Epoxy Resin Kit",
+        price: 4500,
+        peaces: 1
+    }
+];
